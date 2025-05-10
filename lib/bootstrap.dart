a1fe30ae9bccf3e52fb967c28fcaae962b7f328f
Ayabonga <ayabongabooi2@gmail.com>
2025-05-10 22:32:03 +0200

    add missing await

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  runZonedGuarded(() async {
+  await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
     runApp(await builder());
