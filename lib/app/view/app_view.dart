75d7ff806f261b0007d0ae8f295777b1216c50c4
Ayabonga <ayabongabooi2@gmail.com>
2025-05-11 16:19:53 +0200

    wip: setup app theme

--- lib/app/view/app_view.dart
+++ lib/app/view/app_view.dart
@@ -1,11 +1,36 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:mrbooi_eats/home/home.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class AppView extends StatelessWidget {
   const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return ShadApp.material(
+      title: 'MrBooi Eats',
+      themeMode: ThemeMode.light,
+      theme: const AppTheme().theme,
+      darkTheme: const AppDarkTheme().theme,
+      materialThemeBuilder: (context, theme) {
+        return theme.copyWith(
+          appBarTheme: const AppBarTheme(
+            surfaceTintColor: AppColors.transparent,
+          ),
+          textTheme:
+              theme.brightness == Brightness.light
+                  ? const AppTheme().textTheme
+                  : const AppDarkTheme().textTheme,
+          snackBarTheme: const SnackBarThemeData(
+            behavior: SnackBarBehavior.floating,
+          ),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            type: BottomNavigationBarType.fixed,
+          ),
+        );
+      },
+      home: const HomePage(),
+    );
   }
 }
